e136b3a79727edd4c51de75f24a83f071f18f695
Aqib-dev05 <aqibgithub@gmail.com>
2026-03-28 20:27:49 +0500

    production errrr

--- Backend/server.js
+++ Backend/server.js
@@ -5,11 +5,10 @@ import { connectDB } from "./src/config/db.js";
 import { seedAdmin,seedProducts, seedCategory, seedSubCategory } from "./src/seed/seed.js";
 import app from "./src/app.js";
 
-const PORT = process.env.PORT || 5000;
-const MONGO_URI =
-  process.env.MONGO_URI ;
-app.listen(PORT, () => {
-  connectDB(MONGO_URI);
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI ;
+app.listen(PORT,async () => {
+  await connectDB(MONGO_URI);
   seedAdmin();
   seedCategory();
   seedSubCategory();
