2d28c19a51377eefd4bb1ed5a1da6bfcbf98249e
Aqib-dev05 <aqibgithub@gmail.com>
2026-03-30 16:55:45 +0500

    add test route

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -37,4 +37,17 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
+
+app.get("/check",(req,res)=>{
+  res.json({
+    "MONGOI_URI": process.env.MONGO_URI,
+    "PORT":process.env.PORT,
+    "JWT_SECRET":process.env.JWT_SECRET,
+    "CLIENT_ORIGIN":process.env.CLIENT_ORIGIN,
+    "CLOUDINARY_CLOUD_NAME":process.env.CLOUDINARY_CLOUD_NAME,
+    "CLOUDINARY_API_KEY":process.env.CLOUDINARY_API_KEY,
+    "CLOUDINARY_API_SECRET":process.env.CLOUDINARY_API_SECRET
+  })
+})
+
 export default app;
