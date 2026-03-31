e3d8526d2fc24623cdb89e146d5dc00e433838d9
Aqib-dev05 <aqibgithub@gmail.com>
2026-03-31 15:14:29 +0500

    seed data methods modified

--- Backend/server.js
+++ Backend/server.js
@@ -2,19 +2,23 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { connectDB } from "./src/config/db.js";
-import { seedAdmin,seedProducts, seedCategory, seedSubCategory } from "./src/seed/seed.js";
+import {
+  seedAdmin,
+  seedProducts,
+  seedCategory,
+  seedSubCategory,
+} from "./src/seed/seed.js";
 import app from "./src/app.js";
 
 const PORT = process.env.PORT;
-const MONGO_URI = process.env.MONGO_URI ;
-
+const MONGO_URI = process.env.MONGO_URI;
 
 const startServer = async () => {
   await connectDB(MONGO_URI);
-  seedAdmin();
-  seedCategory();
-  seedSubCategory();
-  seedProducts();
+  await seedAdmin();
+  await seedCategory();
+  await seedSubCategory();
+  await seedProducts();
 
   app.listen(PORT, () => {
     console.log(`✅ Server running on port ${PORT}`);
